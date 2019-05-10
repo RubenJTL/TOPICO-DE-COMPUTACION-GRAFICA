ed536837afa28443d2d5bc3fb1ba407e5f71a32f
Ruben Torres Lima <rtorresli@unsa.edu.pe>
2019-05-09 20:16:53 -0500

    Update convolve.py

--- editor/convolve.py
+++ editor/convolve.py
@@ -1,111 +1,61 @@
-
 from __future__ import print_function
+
 import sys
+from timeit import default_timer as timer
+
 import numpy as np
 from scipy.signal import fftconvolve
 from scipy import misc, ndimage
-from accelerate.cuda.fft import FFTPlan, fft_inplace, ifft_inplace
-from numba import cuda, vectorize
-from timeit import default_timer as timer
+from matplotlib import pyplot as plt
 
-@vectorize(['complex64(complex64, complex64)'], target='cuda')
-def vmult(a, b):
-    return a * b
+from accelerate.cuda.fft import FFTPlan
+from numba import cuda
 
 
-def task1(d_image_complex, d_response_complex):
-    ### Task1 ###
-    # Implement a inplace CUDA FFT convolution
-    # Pseduocode:
-    #   freq_imag = fft(image)
-    #   freq_resp = fft(response)
-    #   freq_out = fftimag * fftresp
-    #   output = ifft(freq_out)
-    #
-    # Use the cuFFT functions:
-    #   - fft_inplace(ary)
-    #   - ifft_inplace(ary)
-    #
-    # Call `vmult` which is our elementwise complex multiplication.
-    # Do a inplace operation on `d_image_complex`.
-    # Hints:
-    #   - keyword argument 'out' specify the output array
-    #   - length of d_image_complex and d_response_complex has the same length.
+@cuda.jit('void(complex64[:,:], complex64[:,:])')
+def mult_inplace(img, resp):
+    i, j = cuda.grid(2)
+    if j < img.shape[0] and i < img.shape[1]:
+        img[j, i] *= resp[j, i]
 
 
-    fft_inplace(d_image_complex)
-    fft_inplace(d_response_complex)
-
-    vmult(d_image_complex, d_response_complex, out=d_image_complex)
-
-    ifft_inplace(d_image_complex)
-
-    # At this point, we have applied the filter onto d_image_complex
-    return  # Does not return anything
+def best_grid_size(size, tpb):
+    bpg = np.ceil(np.array(size, dtype=np.float) / tpb).astype(np.int).tolist()
+    return tuple(bpg)
 
 
-# ----------------------------------------------------------------------------
-# Details of the program.  Not necessary for the exercise.
+def convolve(image):
+    kernel = np.array([[1, 0, -1],
+                       [1, 0, -1],
+                       [1, 0, -1]],dtype=np.float32)
 
-def convolve():
-    # Build Filter
-    """laplacian_pts = '''
-    -4 -1 0 -1 -4
-    -1 2 3 2 -1
-    0 3 4 3 0
-    -1 2 3 2 -1
-    -4 -1 0 -1 -4
-    '''.split()
-    laplacian_pts = '''
-    0 0 0 0 0
-    0 0 1 0 0
-    0 1 1 1 0
-    0 0 1 0 0
-    0 0 0 0 0
-    '''.split()
+    # vertical edge detector
+    """kernel = np.array([[1,  1,  1],
+                       [0,  0,  0],
+                       [-1, -1, -1]],dtype=np.float32)
 
-    laplacian_pts = '''
-    1 1 1
-    1 1 1
-    1 1 1
-    '''.split()
-
-    laplacian_pts = '''
-    0 0 0
-    -1 1 0
-    0 0 0
-    '''.split()"""
-
-    laplacian_pts = '''
-    1 2 3 1 1
-    2 7 11 7 2
-    3 11 17 11 3
-    2 7 11 7 1
-    1 2 3 2 1
-    '''.split()
-
-    laplacian = np.array(laplacian_pts, dtype=np.float32).reshape(5, 5)
-
-    image = get_image()
-
-    print("Image size: %s" % (image.shape,))
+    # blurring ("box blur", because it's a box of ones)
+    kernel = np.array([[1, 1, 1],
+                       [1, 1, 1],
+                       [1, 1, 1]],dtype=np.float32) / 9.0
+    """
+    # sharpening
+    kernel = (np.array([[-1, -1, -1],
+                        [-1,  9, -1],
+                        [-1, -1, -1]]))
+    # Build Image
 
     response = np.zeros_like(image)
-    response[:5, :5] = laplacian
-
-    # CPU
-    # Use SciPy to perform the FFT convolution
-    ts = timer()
-    cvimage_cpu = fftconvolve(image, laplacian, mode='same')
-    te = timer()
-    print('CPU: %.2fs' % (te - ts))
+    response[:3, :3] = kernel
 
     # GPU
     threadperblock = 32, 8
     blockpergrid = best_grid_size(tuple(reversed(image.shape)), threadperblock)
     print('kernel config: %s x %s' % (blockpergrid, threadperblock))
 
-    # Initialize the cuFFT system.
+    # Trigger initialization the cuFFT system.
+    # This takes significant time for small dataset.
+    # We should not be including the time wasted here
     FFTPlan(shape=image.shape, itype=np.complex64, otype=np.complex64)
 
     # Start GPU timer
@@ -113,55 +63,61 @@ def convolve():
     image_complex = image.astype(np.complex64)
     response_complex = response.astype(np.complex64)
 
-    d_image_complex = cuda.to_device(image_complex)
-    d_response_complex = cuda.to_device(response_complex)
+    stream1 = cuda.stream()
+    stream2 = cuda.stream()
 
-    task1(d_image_complex, d_response_complex)
+    fftplan1 = FFTPlan(shape=image.shape, itype=np.complex64,
+                       otype=np.complex64, stream=stream1)
+    fftplan2 = FFTPlan(shape=image.shape, itype=np.complex64,
+                       otype=np.complex64, stream=stream2)
 
-    cvimage_gpu = d_image_complex.copy_to_host().real / np.prod(image.shape)
+    # pagelock memory
+    with cuda.pinned(image_complex, response_complex):
+
+        # We can overlap the transfer of response_complex with the forward FFT
+        # on image_complex.
+        d_image_complex = cuda.to_device(image_complex, stream=stream1)
+        d_response_complex = cuda.to_device(response_complex, stream=stream2)
+
+        fftplan1.forward(d_image_complex, out=d_image_complex)
+        fftplan2.forward(d_response_complex, out=d_response_complex)
+
+        stream2.synchronize()
+
+        mult_inplace[blockpergrid, threadperblock, stream1](d_image_complex,
+                                                            d_response_complex)
+        fftplan1.inverse(d_image_complex, out=d_image_complex)
+
+        # implicitly synchronizes the streams
+        cvimage_gpu = d_image_complex.copy_to_host().real / np.prod(image.shape)
 
     te = timer()
     print('GPU: %.2fs' % (te - ts))
 
-    return cvimage_cpu, cvimage_gpu
 
+    return cvimage_gpu
 
-def best_grid_size(size, tpb):
-    bpg = np.ceil(np.array(size, dtype=np.float) / tpb).astype(np.int).tolist()
-    return tuple(bpg)
-
-def get_image():
-    # Build Image
-    try:
-        filename = "goku.jpg"#sys.argv[1]
-        image = ndimage.imread(filename, flatten=True,mode='RGB').astype(np.float32)
-    except IndexError:
-        image = misc.face(gray=True).astype(np.float32)
-    return image
 
 def main():
-    cvimage_cpu, cvimage_gpu = convolve()
 
-    # Plot the results
+    # horizontal edge detector
+
     try:
-        from matplotlib import pyplot as plt
-    except ImportError:
-        print("To render the images")
-        print("Do `conda install matplotlib` on the terminal")
-        print("Then, rerun")
-    else:
-        plt.subplot(1, 2, 1)
-        plt.title('CPU')
-        plt.imshow(cvimage_cpu, cmap=plt.cm.gray)
-        plt.axis('off')
+        filename = "goku.jpg"#sys.argv[1]
+        image = ndimage.imread(filename, flatten=True).astype(np.float32)
+    except IndexError:
+        image = misc.face(gray=True).astype(np.float32)
 
-        plt.subplot(1, 2, 2)
-        plt.title('GPU')
-        plt.imshow(cvimage_gpu, cmap=plt.cm.gray)
-        plt.axis('off')
+    print("Image size: %s" % (image.shape,))
 
-        plt.show()
+    cvimage_gpu=convolve(image)
 
+    plt.subplot(1, 1, 1)
+    plt.title('GPU')
+    plt.imshow(cvimage_gpu, cmap=plt.cm.gray)
+    plt.axis('off')
+
+    plt.show()
 
 if __name__ == '__main__':
     main()
